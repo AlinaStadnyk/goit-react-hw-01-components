bdf049d177c6eb660c00a03d5662647befb9393d
AlinaStadnyk <135226486+AlinaStadnyk@users.noreply.github.com>
2023-11-29 19:11:40 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics title="Upload stats" stats={data} />
+      <Statistics title={'Upload stats'} stats={data} />
       <FriendList friends={friends} />;
       <Transactions items={transactions} />;
     </>
